9ce1b40e7da8a018c64fa9123a84548f0fdbca5a
Samar Rashid <34966884+samar2k17@users.noreply.github.com>
2018-01-19 21:42:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$('h1').hide();
+$('#box1').click(function(){
+    $(this).fadeTo(1000,0.25,function(){
+        
+    })
+    $(this).slideUp();
+    $('h1').slideDown();
+});
+$('button').click(function(){
+    
+})
